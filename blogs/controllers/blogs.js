7501f937a4c8bc2002a9a0516beb7e2538b1979d
Hieu Nguyen <nth937@gmail.com>
2022-06-11 01:08:13 +0300

    testing init

--- blogs/controllers/blogs.js
+++ blogs/controllers/blogs.js
@@ -1,18 +1,5 @@
 const blogsRouter = require('express').Router()
-
-// ______________
-// const mongoose = require('mongoose')
-// const blogSchema = new mongoose.Schema({
-//   title: String,
-//   author: String,
-//   url: String,
-//   likes: Number
-// })
-
-// const Blog = mongoose.model('Blog', blogSchema)
 const Blog = require('../models/blog')
-
-// ______________
 
 blogsRouter.get('/', (request, response) => {
   Blog
