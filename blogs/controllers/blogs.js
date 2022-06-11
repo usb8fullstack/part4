72ea9de6a486d4b59ed53d923aa3787f9ffca1b4
Hieu Nguyen <nth937@gmail.com>
2022-06-11 13:04:07 +0300

    ex4.10

--- blogs/controllers/blogs.js
+++ blogs/controllers/blogs.js
@@ -6,15 +6,15 @@ blogsRouter.get('/', async (request, response) => {
   response.json(blogs)
 })
 
-blogsRouter.post('/', (request, response, next) => {
+blogsRouter.post('/', async (request, response, next) => {
   const blog = new Blog(request.body)
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-    .catch(error => next(error))
+  try {
+    const savedBlog = await blog.save()
+    response.status(201).json(savedBlog)
+  } catch(error) {
+    next(error)
+  }
 })
 
 module.exports = blogsRouter
--- blogs/tests/blog_api.test.js
+++ blogs/tests/blog_api.test.js
@@ -16,31 +16,58 @@ beforeEach(async () => {
   await blogObject.save()
 })
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-}, 100000)
+describe('GET METHOD', () => {
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  }, 100000)
 
-// ---------------------------------------------
-test('all blogs are returned', async () => {
-  const response = await api.get('/api/blogs')
-  expect(response.body).toHaveLength(helper.initialBlogs.length)
+  test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body).toHaveLength(helper.initialBlogs.length)
+  })
+
+  test('a specific blogs is within the returned notes', async () => {
+    const response = await api.get('/api/blogs')
+    const titles = response.body.map(r => r.title)
+    expect(titles).toContain('journal 1')
+  })
+
+  test('verify the existence of property _id', async () => {
+    const response = await api.get('/api/blogs')
+    response.body.forEach(blog => {
+      expect(blog.id).toBeDefined()
+    })
+  })
+
 })
 
-test('a specific blogs is within the returned notes', async () => {
-  const response = await api.get('/api/blogs')
-  const titles = response.body.map(r => r.title)
-  expect(titles).toContain('journal 1')
-})
 
-test('verify the existence of property _id', async () => {
-  const response = await api.get('/api/blogs')
-  response.body.forEach(blog => {
-    expect(blog.id).toBeDefined()
+describe('POST METHOD', () => {
+  test('a valid blog can be added ', async () => {
+    const newBlog = {
+      title: 'journal 3',
+      author: 'hihi',
+      url: 'http://www.hihi.com',
+      likes: 3,
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+
+    const titles = blogsAtEnd.map(n => n.title)
+    expect(titles).toContain('journal 3')
   })
 })
+
 
 // ---------------------------------------------
 afterAll(() => {
