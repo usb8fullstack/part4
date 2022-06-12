2c2bd0ecd8aa4b044b25ddfe0ff4377c26a5db92
Hieu Nguyen <nth937@gmail.com>
2022-06-12 13:31:48 +0300

    ex4.18 & 4.19

--- blogs/controllers/blogs.js
+++ blogs/controllers/blogs.js
@@ -2,6 +2,15 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
 
+const jwt = require('jsonwebtoken')
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
+
 blogsRouter.get('/', async (request, response) => {
   const blogs =  await Blog.find({}).populate('user',
     { username: 1, name: 1, id: 1 }
@@ -12,23 +21,33 @@ blogsRouter.get('/', async (request, response) => {
 blogsRouter.post('/', async (request, response, next) => {
   const body = request.body
 
-  const user = await User.findById(body.userId)
-
-  if (!body.title || !body.author || !body.url) {
-    return response.status(400).json({
-      error: 'title, author or url must be not emty'
-    })
+  const token = getTokenFrom(request)
+  if (!token) {
+    return response.status(401).json({ error: 'token must be provided' })
   }
 
-  const blog = new Blog({
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes || 0,
-    user: user._id
-  })
-
+  // If there is no token passed, it will return error "jwt must be provided".
   try {
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: 'token missing or invalid' })
+    }
+    const user = await User.findById(decodedToken.id)
+
+    if (!body.title || !body.author || !body.url) {
+      return response.status(400).json({
+        error: 'title, author or url must be not emty'
+      })
+    }
+
+    const blog = new Blog({
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes || 0,
+      user: user._id
+    })
+
     const savedBlog = await blog.save()
 
     // user.blogs = user.blogs.concat(savedBlog._id)
