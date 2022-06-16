00a881fc1c442c559a9906a810a5435759d228d1
Hieu Nguyen <nth937@gmail.com>
2022-06-17 00:27:46 +0300

    for part 5 testing

--- blogs/app.js
+++ blogs/app.js
@@ -32,6 +32,11 @@ app.use('/api/login', loginRouter)
 app.use('/api/blogs', blogsRouter)
 // app.use('/api/blogs', middleware.userExtractor, blogsRouter)
 
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/testing')
+  app.use('/api/testing', testingRouter)
+}
+
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
